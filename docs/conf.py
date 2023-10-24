55d03d8425e318d9114f42da806a84ebe3eac6b1
Quentame <polletquentin74@me.com>
2023-10-24 07:31:51 +0000

    Uniform doc config

--- docs/conf.py
+++ docs/conf.py
@@ -2,12 +2,12 @@
 from datetime import datetime
 
 
-project = "freebox-api"
-author = "HACF"
+project = "Freebox API"
+author = "HACF (created by @fstercq, maintained by @Quentame)"
 copyright = f"{datetime.now().year}, {author}"
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon"
+    "sphinx.ext.napoleon",
 ]
 autodoc_typehints = "description"
 html_theme = "furo"
